1b8b2a068b89e5f3a18ecf18c1021b28b426e3c8
nataliet57 <nataliewytam@gmail.com>
2018-11-08 12:25:45 -0500

    created first test avg method

--- statTools.py
+++ statTools.py
@@ -1,1 +1,11 @@
+def testavg(myString):
 
+    if(len(myString) < 1 ):
+        print("Please enter a list of numbers")
+    elif(len(myString == 1)):
+        print(myString)
+
+    else:
+        for i in range (len(myString)):
+            sum += myString[i]
+        avg = sum/(len(myString))
