dcf5d4e3ca6147fb7b899377e743be50aa806992
Jason-Ng <jason.ng19@ycdsbk12.ca>
2018-11-09 11:04:05 -0500

    Code has a better header

--- statTools.py
+++ statTools.py
@@ -1,5 +1,15 @@
-import math
+"""
+------------------------------------------------------------------------------------------------------------------------
+Name: statTools.py
+Purpose:
+    Collection of functions calculating the central tendencies and spread of a set of numerical data
+    - mean, median, mode - range, upper quartile, lower quartile, variance, standard deviation
+Authors: Ng.J, Tam.N
+Created: 2018/11/11
+------------------------------------------------------------------------------------------------------------------------
+"""
 
 numberList = [1,2,3,4,5,5,6,7,8,9]
 
 def mode(numberList):
+    
