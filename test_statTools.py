d8ea5052e9aab610a337b24e092c927da389e767
Jason-Ng <jason.ng19@ycdsbk12.ca>
2018-11-10 02:14:50 -0500

    create test code

--- test_statTools.py
+++ test_statTools.py
@@ -2,3 +2,10 @@
 import pytest
 from statTools import *
 
+rangeList1 = [1,2,3,4,24,537,37,16, 4]
+rangeList2 = [11,15,16,5,28,64,845,82,25]
+rangeList3 = []
+
+def test_range_1():
+    print(range(rangeList1))
+
